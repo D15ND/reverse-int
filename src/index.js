c2b71862e9205a1b70dd878505fc29037ca22ec6
ILYA <106765406+D15ND@users.noreply.github.com>
2023-01-09 00:36:13 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let numAbs=Math.abs(n);
+  let nToStr=numAbs.toString();
+  let rev=nToStr.split('').reverse().join('');
+  let revToNum=+rev;
+  return revToNum;
 }
